google/rekall

commit 5d95552527d1e547fcfe0ba63365c6874e2ba210

Moved all file open operations to the renderer.
Rekall plugins should never attempt to open files on their own since, when used as a library they may not actually have access to the filesystem.

BUG=
R=parki.san@gmail.com

Review URL: https://codereview.appspot.com/109990043

rekall/plugins/tools/mspdb.py
@@ -171,11 +171,12 @@ class FetchPDB(core.DirectoryDumperMixin, plugin.Command):
                 data = urllib2.urlopen(request).read()
                 renderer.format("Received {0} bytes\n", len(data))
 
-                output_file = os.path.join(self.dump_dir, "%s.pd_" % basename)
-                with open(output_file, "wb") as fd:
+                with renderer.open(filename="%s.pd_" % basename,
+                                   directory=self.dump_dir) as fd:
                     fd.write(data)
 
                 try:
+                    output_file = os.path.join(self.dump_dir, )
                     subprocess.check_call(["cabextract",
                                            os.path.basename(output_file)],
                                           cwd=self.dump_dir)

rekall/plugins/core.py
@@ -515,10 +515,9 @@ class OutputFileMixin(object):
 
     def __init__(self, out_file=None, **kwargs):
         super(OutputFileMixin, self).__init__(**kwargs)
+        self.out_file = out_file
         if out_file is None:
             raise RuntimeError("An output must be provided.")
-
-        self.output = open(out_file, mode="w")
 
 
 class DirectoryDumperMixin(object):

rekall/plugins/darwin/pslist.py
@@ -182,7 +182,9 @@ class DarwinVadDump(core.DirectoryDumperMixin, common.DarwinProcessFilter):
                 renderer.format(u"Writing {0}, pid {1} to {2}\n",
                                 proc.p_comm, proc.p_pid, filename)
 
-                with open(os.path.join(self.dump_dir, filename), 'wb') as fd:
+                with renderer.open(directory=self.dump_dir,
+                                   filename=filename,
+                                   mode='wb') as fd:
                     self.CopyToFile(task_space, vma.links.start,
                                     vma.links.end, fd)
 

rekall/plugins/imagecopy.py
@@ -80,9 +80,10 @@ class ImageCopy(plugin.PhysicalASMixin, plugin.Command):
                                      "please remove it before continuing")
 
         blocksize = 1024 * 1024 * 5
-        with open(self.output_image, "wb") as fd:
+        with renderer.open(filename=self.output_image,
+                           mode="wb") as fd:
             for _ in self.address_space.get_available_addresses():
-                range_offset, phys_range_offset, range_length = _
+                range_offset, _, range_length = _
                 renderer.format("Range {0:#x} - {1:#x}\n",
                                 range_offset, range_length)
 

rekall/plugins/linux/lsmod.py
@@ -275,8 +275,11 @@ class Moddump(common.LinuxPlugin):
         lsmod_plugin = self.session.plugins.lsmod(session=self.session)
         for module in lsmod_plugin.get_module_list():
             file_name = "{0}.{1:#x}.lkm".format(module.name, module.module_core)
-            mod_file = open(os.path.join(self.dump_dir, file_name), 'wb')
-            mod_data = self.dump_module(module)
-            mod_file.write(mod_data)
-            mod_file.close()
-            renderer.write("Wrote {0} bytes to {1}\n".format(module.core_size, file_name))
+            with renderer.open(directory=self.dump_dir,
+                               filename=file_name,
+                               mode="wb") as mod_file:
+
+                mod_data = self.dump_module(module)
+                mod_file.write(mod_data)
+                renderer.format("Wrote {0} bytes to {1}\n",
+                                module.core_size, file_name)

rekall/plugins/linux/netstat.py
@@ -132,7 +132,7 @@ class PacketQueues(common.LinuxPlugin):
         super(PacketQueues, self).__init__(**kwargs)
         self.output_dir = dump_dir
 
-    def process_socket(self, task, fd_num, socket):
+    def process_socket(self, renderer, task, fd_num, socket):
         queues = ["receive", "write"]
         for queue_name in queues:
             sk_buff_head_name = "sk_{0:s}_queue".format(queue_name)
@@ -148,10 +148,13 @@ class PacketQueues(common.LinuxPlugin):
                     data.append(self.kernel_address_space.read(
                         sk_buff.data.obj_offset, pkt_len))
             if data:
-                with open(os.path.join(self.output_dir, filename), "wb") as fd:
+                with renderer.open(directory=self.output_dir,
+                                   filename=filename,
+                                   mode="wb") as fd:
                     fd.write(''.join(data))
                     logging.debug("Wrote %d bytes to %s", fd.tell(), filename)
                 return True
+
             else:
                 logging.debug("Skipped empty queue %s", filename)
                 return False
@@ -162,8 +165,8 @@ class PacketQueues(common.LinuxPlugin):
 
         all_sockets = list(sockets)
         skipped_sockets_count = 0
-        for task, fd, socket, iaddr in all_sockets:
-            if not self.process_socket(task, fd, socket):
+        for task, fd, socket, _ in all_sockets:
+            if not self.process_socket(renderer, task, fd, socket):
                 skipped_sockets_count += 1
         renderer.format("Skipped %d/%d sockets.", skipped_sockets_count,
                         len(all_sockets))

rekall/plugins/linux/proc_maps.py
@@ -22,7 +22,6 @@
 @contact:      atcuno@gmail.com
 @organization: Digital Forensics Solutions
 """
-import os
 
 from rekall import testlib
 from rekall.plugins import core
@@ -102,7 +101,6 @@ class LinVadDump(core.DirectoryDumperMixin, common.LinProcessFilter):
             # Get the task and all process specific information
             task_space = task.get_process_address_space()
             name = task.comm
-            offset = task.obj_offset
 
             for vma in task.mm.mmap.walk_list("vm_next"):
                 if not vma.vm_file:
@@ -114,7 +112,9 @@ class LinVadDump(core.DirectoryDumperMixin, common.LinProcessFilter):
                 renderer.format(u"Writing {0}, pid {1} to {2}\n",
                                 task.comm, task.pid, filename)
 
-                with open(os.path.join(self.dump_dir, filename), 'wb') as fd:
+                with renderer.open(directory=self.dump_dir,
+                                   filename=filename,
+                                   mode='wb') as fd:
                     self.CopyToFile(task_space, vma.vm_start, vma.vm_end, fd)
 
 

rekall/plugins/linux/pslist.py
@@ -22,8 +22,6 @@
 @contact:      atcuno@gmail.com
 @organization: Digital Forensics Solutions
 """
-import os
-
 from rekall import plugin
 from rekall import testlib
 from rekall.plugins import core
@@ -120,13 +118,14 @@ class LinMemDump(core.DirectoryDumperMixin, LinMemMap):
             raise plugin.PluginError("Dump directory not specified.")
 
         for task in self.filter_processes():
-            filename = os.path.join(
-                self.dump_dir, u"{0}_{1:d}.dmp".format(task.comm, task.pid))
+            filename = u"{0}_{1:d}.dmp".format(task.comm, task.pid)
 
             renderer.write(u"Writing {0} {1:6x} to {2}\n".format(
                     task.comm, task, filename))
 
-            with open(filename, 'wb') as fd:
+            with renderer.open(directory=self.dump_dir,
+                               filename=filename,
+                               mode='wb') as fd:
                 maps = self.dump_process(task, fd)
 
             temp_renderer = text.TextRenderer(session=self.session,

commit e80b75abc8fe6d2fd48ee7157dbdab147258397d

Bug fix for fetch_pdb caused by regression from 5d95552527d1e547fcfe0ba63365c6874e2ba210
BUG=
R=parki.san@gmail.com

Review URL: https://codereview.appspot.com/111840043

rekall/plugins/tools/mspdb.py
@@ -166,17 +166,18 @@ class FetchPDB(core.DirectoryDumperMixin, plugin.Command):
 
                 renderer.format("Trying to fetch {0}\n", url)
                 request = urllib2.Request(url, None, headers={
-                        'User-Agent': self.USER_AGENT})
+                    'User-Agent': self.USER_AGENT})
 
                 data = urllib2.urlopen(request).read()
                 renderer.format("Received {0} bytes\n", len(data))
 
-                with renderer.open(filename="%s.pd_" % basename,
-                                   directory=self.dump_dir) as fd:
+                output_file = os.path.join(self.dump_dir, "%s.pd_" % basename)
+                with renderer.open(filename=output_file,
+                                   directory=self.dump_dir,
+                                   mode="wb") as fd:
                     fd.write(data)
 
                 try:
-                    output_file = os.path.join(self.dump_dir, )
                     subprocess.check_call(["cabextract",
                                            os.path.basename(output_file)],
                                           cwd=self.dump_dir)